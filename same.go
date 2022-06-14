c3c0cb652694afc8f17e2203a0e9cd5ab19a6974
raushan211 <raazcrzy90@gmail.com>
2022-06-14 17:14:15 +0530

    same

---
+++ same.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var word1 = []string{"ab", "c"}
+	var word2 = []string{"a", "bc"}
+	var a string
+	var b string
+
+	for i := 0; i < len(word1); i++ {
+		a = a + word1[i]
+
+	}
+	for j := 0; j < len(word2); j++ {
+		b = b + word2[j]
+
+	}
+
+	if a == b {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+
+}
