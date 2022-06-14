da7cb54e0afb1b366b60a6527d81a2537d077d6e
raushan211 <raazcrzy90@gmail.com>
2022-06-14 17:59:25 +0530

    same

--- same.go
+++ same.go
@@ -1,26 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var word1 = []string{"ab", "c"}
 	var word2 = []string{"a", "bc"}
-	var a string
-	var b string
+	// var a string
+	// var b string
 
-	for i := 0; i < len(word1); i++ {
-		a = a + word1[i]
+	// for i := 0; i < len(word1); i++ {
+	// 	a = a + word1[i]
 
-	}
-	for j := 0; j < len(word2); j++ {
-		b = b + word2[j]
+	// }
+	// for j := 0; j < len(word2); j++ {
+	// 	b = b + word2[j]
 
-	}
+	// }
 
-	if a == b {
-		fmt.Println("true")
-	} else {
-		fmt.Println("false")
-	}
+	// if a == b {
+	// 	fmt.Println("true")
+	// } else {
+	// 	fmt.Println("false")
+	// }
 
+	fmt.Println(strings.Join(word1, "") == strings.Join(word2, ""))
 }
